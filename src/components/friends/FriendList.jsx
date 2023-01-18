8c1071ade7085ca527db115add51d878fd1fc665
Bohdan <666lordlir999@gmail.com>
2023-01-18 18:57:45 +0100

    сщььше

--- src/components/friends/FriendList.jsx
+++ src/components/friends/FriendList.jsx
@@ -1,25 +1,25 @@
 export const FriendList = ({ friends }) => {
     return (
 
-        <ul class="friend-list">
+        <ul className="friend-list">
             {friends.map(({
 avatar,
 name,
 isOnline,
-id,}) => <li class="item" key={id}>
-                    {isOnline ? <span style={{ backgroundColor: "green" , width: 10 ,height:10, display: 'block' } }class="status"></span> : <span style={{backgroundColor: "red", width: 10 ,height:10, display: 'block'} }class="status"></span>}
-                <img class="avatar" src={avatar} alt={name} width="48" />
-                    <p class="name">{ name}</p>
+id,}) => <li className="item" key={id}>
+                    {isOnline ? <span style={{ backgroundColor: "green" , width: 10 ,height:10, display: 'block' } }className="status"></span> : <span style={{backgroundColor: "red", width: 10 ,height:10, display: 'block'} }className="status"></span>}
+                <img className="avatar" src={avatar} alt={name} width="48" />
+                    <p className="name">{ name}</p>
             </li>)}
 
 
         </ul>
-        // <ul className="stat-list">
+        // <ul classNameName="stat-list">
         //     {stats.map(({id, label, percentage}) => {
         //         return (
-        //             <li className="item" key={id}>
-        //                 <span className="label">{ label }</span>
-        //                 <span className="percentage">{ percentage }</span>
+        //             <li classNameName="item" key={id}>
+        //                 <span classNameName="label">{ label }</span>
+        //                 <span classNameName="percentage">{ percentage }</span>
         //             </li>)
         //         })}
 
--- src/components/satistic/Statistics.jsx
+++ src/components/satistic/Statistics.jsx
@@ -1,13 +1,14 @@
 import stat from './statistic.module.css'
+import { getRandomHexColor } from './getRandomBgCol'
 
 export const Statistics = ({ stats, title }) => {
     return (
     <section className={stat.statistics}>
             {title && (<h2 className="title">Upload stats</h2>)}
-        <ul className={stats.statList}>
+        <ul className={stat.statList}>
             {stats.map(({id, label, percentage}) => {
                 return (
-                    <li className="item" key={id}>
+                    <li style={{ backgroundColor: getRandomHexColor()  }} className={stat.item} key={id}>
                         <span className="label">{ label }</span>
                         <span className="percentage">{ percentage }</span>
                     </li>)
